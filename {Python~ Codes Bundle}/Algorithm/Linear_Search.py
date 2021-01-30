2825ca34c130c31b81ce9b565740d3e89b35ef0c
XJefferson0101 <71709664+XJefferson0101@users.noreply.github.com>
2021-01-30 09:10:21 +0530

    3
    3

--- {Python~ Codes Bundle}/Algorithm/Linear_Search.py
+++ {Python~ Codes Bundle}/Algorithm/Linear_Search.py
@@ -1,8 +1,8 @@
-lst=[]
-for lst_l in range(5):
-	lst_n = int(input(": "))
-	lst.append(lst_n)
-lst_len=len(lst)
+lst=[]  # Create an empty list[] 
+for lst_l in range(5):  # range of the list[]
+	lst_n = int(input(": "))  # Elements to be put to the the list[]
+	lst.append(lst_n)  
+lst_len=len(lst)  # legth of the list[]
 k=0;j=0
 n=int(input("Enter num to be search: "))
 for i in range(0,lst_len):
