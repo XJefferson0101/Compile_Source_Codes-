a01a21cde96072312b09b4a48f9ae09a3eefc064
XJefferson0101 <71709664+XJefferson0101@users.noreply.github.com>
2021-03-06 07:42:49 +0530

    19
    19

--- {XI ~ Python Learning}/List Manipulation [ ]/Q1~.py
+++ {XI ~ Python Learning}/List Manipulation [ ]/Q1~.py
@@ -1,12 +1,11 @@
-lst = []; count = 1; new_lst = [] 
-lst_range = int(input(" Enter the range of a List[]: "))
+arr = []; new_arr = [] 
+lst_range = int(input('Enter the range of a list: '))
 for i in range(0,lst_range):
-	n = int(input(" Enter an Integer: ")) 
-	lst.append(n)
-lst_len = len(lst)
-for j in range(0,lst_len):
-	count+=j
-	get_var = lst[lst_len-count]
-	new_lst.append(get_var)
-print(' Given List[]: ',lst)
-print('List[] after reversing: ',new_lst)
+	n = int(input('Enter an integer: '))
+	arr.append(n)
+count=1 
+while lst_range>0:
+	lst_range-=count
+	new_arr.append(arr[lst_range])
+print('Given array: ',arr)
+print('Array after reversing: ',new_arr)
